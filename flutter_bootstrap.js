cc31231c570a7a46616d782099249b68365960fa
khalidelasrii <khalidelasri534@gmail.com>
2024-08-12 01:18:59 +0100

    builmeet v1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4221572265"
+    serviceWorkerVersion: "478339096"
   }
 });
